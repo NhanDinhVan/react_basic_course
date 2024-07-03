b31f3e00cbe809bed7a38096c4c76f650cc260a1
DinhVanNhan <vannhan10102004@gmail.com>
2024-07-03 16:45:17 +0700

    Form in reactJs

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -2,28 +2,41 @@ import React from "react";
 
 class MyComponent extends React.Component {
     state = {
-        name: "Nhan dinh",
-        chanel: "NhanDinhVan"
+        firstName: "",
+        lastName: ""
     }
-    handleOnChangeName = (event) => {
+    onChangeFirstName = (event) => {
         this.setState({
-            name: event.target.value
+            firstName: event.target.value
         })
     }
-
-    handleClickButton = () => {
-        alert("Click me")
+    onChangeLastName = (event) => {
+        this.setState({
+            lastName: event.target.value
+        })
     }
+    onClickMe = (event) => {
+        event.preventDefault()
+        alert("Click Me ?")
+    }
+
     render() {
-        return <h3>
-            <input value={this.state.name}
-                onChange={(event) => this.handleOnChangeName(event)
-                } />
-            {console.log("my name is: ", this.state.name)}
-            <p>Name: {this.state.name}</p>
-            <p>Chanel: {this.state.chanel}</p>
-            <button onClick={() => this.handleClickButton()}>Click me</button>
-        </h3>
+        return <>
+            <form >
+                <label>First name: {this.state.firstName}</label><br />
+                <input type="text"
+                    value={this.state.firstName}
+                    onChange={(event) => this.onChangeFirstName(event)}
+                ></input><br />
+                <label>Last name: {this.state.lastName}</label><br />
+                <input type="text"
+                    value={this.state.lastName}
+                    onChange={(event) => this.onChangeLastName(event)}
+                ></input><br /><br />
+                <input type="button" value="Submit"
+                    onClick={(event) => this.onClickMe(event)}></input>
+            </form>
+        </>
     }
 }
 export default MyComponent;

a9fafd9e6dd505735439d8037d2148f68fe4773a
DinhVanNhan <vannhan10102004@gmail.com>
2024-07-21 22:27:12 +0700

    Conditional output

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 class ChildComponent extends React.Component {
-    // state = {
-    //     firstName: "",
-    //     lastName: ""
-    // }
+    state = {
+        isShow: false
+    }
+    onChangeHideShowState = (state) => {
+        this.setState({
+            isShow: !this.state.isShow
+        })
+    }
     // onChangeFirstName = (event) => {
     //     this.setState({
     //         firstName: event.target.value
@@ -24,19 +28,29 @@ class ChildComponent extends React.Component {
         // let name = this.props.name
         // let age = this.props.age
         const { firstName, lastName, classList } = this.props;
+        let { isShow } = this.state
         console.log(">>> Check props: " + this.props)
         return <>
-            <div className="my-3">
-                Full Name: {firstName + " " + lastName}
+            <div className="my-3 ">
+                <h3><i className="m-3">Full Name: </i><i className="text-primary">{firstName + " " + lastName}</i></h3>
             </div>
             <div>
-                <h3 className="text-warning my-3">Your class:</h3>
+                <h3 className="my-3">
+                    <i className="m-3">Your class:</i>
+                    <button onClick={() => this.onChangeHideShowState()}>Hide&Show</button>
+                </h3>
                 {
                     classList.map((item, classList) => {
                         return (
-                            <div key={item.className}>
-                                {item.className} - {item.teacherName}
-                            </div>
+                            <>
+                                {
+                                    isShow &&
+                                    <div className="text-primary" key={item.className}>
+                                        {item.className} - {item.teacherName}
+                                    </div>
+                                }
+
+                            </>
                         )
                     })
                 }
--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -26,27 +26,39 @@ class MyComponent extends React.Component {
 
     render() {
         return <>
-            <form >
-                <label>First name:</label><br />
-                <input type="text"
-                    value={this.state.firstName}
-                    onChange={(event) => this.onChangeFirstName(event)}
-                ></input><br />
-                <label>Last name:</label><br />
-                <input type="text"
-                    value={this.state.lastName}
-                    onChange={(event) => this.onChangeLastName(event)}
-                ></input><br /><br />
-                <input type="button" value="Submit"
-                    className="d-flex justify-content-center align-center"
-                    onClick={(event) => this.onClickMe(event)}></input>
-            </form>
-            <ChildComponent
-                firstName={this.state.firstName}
-                lastName={this.state.lastName}
-                classList={this.state.classList}
-            ></ChildComponent>
+            <div className="col-12 d-flex ">
+                <div className="col-6 d-flex">
+                    <form className="d-flex justify-content-center flex-wrap">
+                        <div>
+                            <label>First name:</label><br />
+                            <input type="text"
+                                value={this.state.firstName}
+                                onChange={(event) => this.onChangeFirstName(event)}
+                                className=""
+                            ></input><br />
+                        </div>
+                        <div>
 
+                            <label>Last name:</label><br />
+                            <input type="text"
+                                value={this.state.lastName}
+                                onChange={(event) => this.onChangeLastName(event)}
+                            ></input><br /><br />
+                        </div>
+                        <input type="button" value="Submit"
+                            className="col-6"
+                            onClick={(event) => this.onClickMe(event)}></input>
+                    </form>
+                </div>
+
+                <div className="col-6">
+                    <ChildComponent
+                        firstName={this.state.firstName}
+                        lastName={this.state.lastName}
+                        classList={this.state.classList}
+                    ></ChildComponent>
+                </div>
+            </div>
         </>
     }
 }
